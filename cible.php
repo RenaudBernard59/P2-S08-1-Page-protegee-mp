8a539bfe8c316a6bd55dce6471ba78866c3b27cb
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-07 14:38:23 +0200

    Creating and editing cible.php It can echo denied access version page

---
+++ cible.php
@@ -0,0 +1,81 @@
+<?php
+
+if ((htmlspecialchars($_POST['password'])) == "password") {
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Le boss final des internet</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+       <h1>Le boss final des internet</h1>
+    </header>
+    
+    <!-- Le corps -->
+    
+    <main>
+
+    <h1>WARNING ! browser ERROR</h1>
+
+    <p><strong>Ah c'est con !!!</strong><br/>Cette page a été optimisée pour Internet Explorer sur Macintoch et ne peut donc pas s'afficher sur votre appareil. 
+    </p>
+     
+    
+    </main>
+    
+    <!-- Le pied de page -->
+    
+    <footer id="pied_de_page">
+        <p>Copyright 20147 - Renaud BERNARD</p>
+    </footer>
+    
+    </body>
+</html>
+
+<?php
+} else {
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Le boss final des internet</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+       <h1>Le boss final des internet</h1>
+    </header>
+    
+    <!-- Le corps -->
+    
+    <main>
+
+    <p>Access denied !</p>
+    <p><a href="index.php" title="Revenir à la page précédente">Come back</a></p>
+      
+    </main>
+    
+    <!-- Le pied de page -->
+    
+    <footer id="pied_de_page">
+
+        <p>Copyright 20147 - Renaud BERNARD</p>
+    </footer>
+    
+    </body>
+</html>
+<?
+};
+
