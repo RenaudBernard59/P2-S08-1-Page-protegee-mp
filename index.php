a1614e166526e529084258b634a5de86a1f43bad
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-07 12:08:50 +0200

    Adding HTML with form at index.php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<?php echo 'Hello world!'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Page d'accès au boss final des internet</title>
+    </head>
+ 
+    <body>
+ 
+    <!-- L'en-tête -->
+    
+    <header>
+       <h1>Page d'acès au boss final des internet</h1>
+    </header>
+    
+    <!-- Le corps -->
+    
+    <main>
+        <h1>Mon super site</h1>
+        
+        <p>Grâce au mot de passe "password" vous pourrez accéder au boss final des internet qui vous révéleras la réponse à la grande question sur la vie, l'univers et le reste ainsi que le fameux secret perdu de l'acier</p>
+
+	<form method="post" action="cible.php">
+	 <label for="pass">Le mot de passe :</label>
+      	 <input type="password" name="pass" id="pass" />
+	<input type="submit" value="Envoyer" />
+	</form>
+	<p><small>Attention toute cela  est fortement confidentiel et ne dois absolument pas être révéler</small></p>
+    </main>
+    
+    <!-- Le pied de page -->
+    
+    <footer id="pied_de_page">
+        <p>Copyright 20147 - Renaud BERNARD</p>
+    </footer>
+    
+    </body>
+</html>
+
